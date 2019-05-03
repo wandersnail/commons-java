988ce0eea184af0bef3c280292540e59079eae13
fszeng2011 <zeng_fs@yeah.net>
2019-05-03 14:54:35 +0800

    新增扩展函数：获取文件MD5值

--- app/src/main/java/com/snail/java/example/Example.kt
+++ app/src/main/java/com/snail/java/example/Example.kt
@@ -3,7 +3,9 @@ package com.snail.java.example
 import com.snail.java.network.NetworkRequester
 import com.snail.java.network.callback.RequestCallback
 import com.snail.java.network.converter.StringResponseConverter
+import com.snail.java.utils.getMD5Code
 import com.snail.java.utils.toDetailMsg
+import java.io.File
 
 /**
  *
@@ -11,21 +13,27 @@ import com.snail.java.utils.toDetailMsg
  * date: 2019/5/1 18:33
  * author: zengfansheng
  */
-class Example {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            NetworkRequester.get("https://www.baidu.com", StringResponseConverter(), object : RequestCallback<String> {
-                override fun onSuccess(parsedResp: String) {
-                    println(parsedResp)
-                    System.exit(0)
-                }
+object Example {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        
+    }
 
-                override fun onError(t: Throwable) {
-                    println(t.toDetailMsg())
-                    System.exit(0)
-                }
-            })
-        }
+    private fun testNetwork() {
+        NetworkRequester.get("https://www.baidu.com", StringResponseConverter(), object : RequestCallback<String> {
+            override fun onSuccess(parsedResp: String) {
+                println(parsedResp)
+                System.exit(0)
+            }
+
+            override fun onError(t: Throwable) {
+                println(t.toDetailMsg())
+                System.exit(0)
+            }
+        })
+    }
+    
+    private fun testFileMD5() {
+        println(File("C:\\Users\\zeng_\\Desktop\\血族.The.Strain.S04E04.中英字幕.WEBRip.AAC.720p.x264-人人影视.mp4").getMD5Code())
     }
 }
